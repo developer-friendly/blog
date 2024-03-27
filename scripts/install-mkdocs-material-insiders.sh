14789d3d181d12bab2e1a749c4edf957e2ef3721
Meysam Azad <meysam@developer-friendly.blog>
2024-03-27 15:49:04 +0700

    feat: switch to mkdocs-material-insiders

---
+++ scripts/install-mkdocs-material-insiders.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eux
+
+version=${1:-''}
+
+if [ -z "$version" ]; then
+  echo "Using latest version of mkdocs-material-insiders"
+  version_suffix=""
+else
+  echo "Using version $version of mkdocs-material-insiders"
+  version_suffix="@$version"
+fi
+
+pip install "mkdocs-material[imaging] @ git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git${version_suffix}"
