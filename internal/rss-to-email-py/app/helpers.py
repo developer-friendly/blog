a7e665920dfea96a7ad9186f20b49ba8a22b797e
Meysam Azad <meysam@developer-friendly.blog>
2024-05-27 14:13:54 +0700

    fix(internal): watch out for the possible current monday

--- internal/rss-to-email-py/app/helpers.py
+++ internal/rss-to-email-py/app/helpers.py
@@ -5,7 +5,10 @@ from datetime import datetime, timedelta
 
 def next_monday():
     today = datetime.now()
-    next_monday = today + timedelta(days=(7 - today.weekday()))
+    if today.weekday() != 0:
+        next_monday = today + timedelta(days=(7 - today.weekday()))
+    else:
+        next_monday = today
     send_at = next_monday.replace(hour=8, minute=0, second=0)
 
     return send_at
