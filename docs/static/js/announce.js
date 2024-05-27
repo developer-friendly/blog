187173b44e032cb1820c8453e3365d30066a1c1c
Meysam Azad <meysam@developer-friendly.blog>
2024-05-27 09:53:02 +0700

    feat(mkdocs): add sponsor announcement

---
+++ docs/static/js/announce.js
@@ -0,0 +1,33 @@
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 != currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+async function showRandomAnnouncement(groupId, timeInterval) {
+  var announcement = document.getElementById(groupId);
+  if (announcement) {
+    var children = [].slice.call(announcement.children);
+    children = shuffle(children);
+    var index = 0;
+    function announceRandom() {
+      children.forEach(function displayNone(el, i) {
+        el.style.display = "none";
+      });
+      children[index].style.display = "block";
+      index = (index + 1) % children.length;
+    }
+    announceRandom();
+    setInterval(announceRandom, timeInterval);
+  }
+}
+
+showRandomAnnouncement("announce-left", 5000);
