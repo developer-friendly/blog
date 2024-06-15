33db732b608dc9b6d1534e41e74ecbda07799f95
Meysam Azad <meysam@developer-friendly.blog>
2024-06-15 11:14:14 +0700

    fix: add hooks directory

--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -7,6 +7,7 @@ x_intent = "https://twitter.com/intent/tweet"
 x_id = "@devfriendly_"
 fb_sharer = "https://www.facebook.com/sharer/sharer.php"
 lnkd_sharer = "https://www.linkedin.com/sharing/share-offsite/"
+reddit_sharer = "https://www.reddit.com/submit"
 include = re.compile(r"[1-9].*")
 
 
@@ -23,6 +24,7 @@ def on_page_markdown(markdown, **kwargs):
     return markdown + dedent(
         f"""
     [Share on :simple-linkedin:]({lnkd_sharer}?url={page_url}){{ .md-button .md-button--primary }}
+    [Share on :simple-reddit:]({reddit_sharer}?url={page_url}&title={_page_title}){{ .md-button .md-button--primary }}
     [Share on :simple-x:]({x_intent}?text={page_title_x}&url={page_url}){{ .md-button .md-button--primary }}
     [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button .md-button--primary }}
     """
