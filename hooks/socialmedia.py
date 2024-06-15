a90c11e317e865e3a406dd57ed5c3261148a7468
Meysam Azad <meysam@developer-friendly.blog>
2024-06-15 12:11:42 +0700

    chore(hooks): rename used variable

--- hooks/socialmedia.py
+++ hooks/socialmedia.py
@@ -18,13 +18,13 @@ def on_page_markdown(markdown, **kwargs):
         return markdown
 
     page_url = config.site_url + page.url
-    _page_title = urllib.parse.quote(page.title + "\n")
+    page_title = urllib.parse.quote(page.title + "\n")
     page_title_x = urllib.parse.quote(f"{x_id}\n{page.title}\n")
 
     return markdown + dedent(
         f"""
     [Share on :simple-linkedin:]({lnkd_sharer}?url={page_url}){{ .md-button .md-button--primary }}
-    [Share on :simple-reddit:]({reddit_sharer}?url={page_url}&title={_page_title}){{ .md-button .md-button--primary }}
+    [Share on :simple-reddit:]({reddit_sharer}?url={page_url}&title={page_title}){{ .md-button .md-button--primary }}
     [Share on :simple-x:]({x_intent}?text={page_title_x}&url={page_url}){{ .md-button .md-button--primary }}
     [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button .md-button--primary }}
     """
