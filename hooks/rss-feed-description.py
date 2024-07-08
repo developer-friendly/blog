b87d7e18d696fb9a08310f4cc7aaae1395761f86
Meysam Azad <meysam@developer-friendly.blog>
2024-07-08 09:07:07 +0700

    chore: customize rss feed with the excerpt of the post

---
+++ hooks/rss-feed-description.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import re
+from markdown import markdown as md_maker
+
+include = re.compile(r"[1-9].*")
+
+
+def on_page_markdown(markdown, page, config, files):
+    if not include.match(page.url):
+        return markdown
+
+    abstract_delimiter = config.plugins["rss"].config["abstract_delimiter"]
+    excerpt_index = markdown.find(abstract_delimiter)
+    if excerpt_index:
+        excerpt = md_maker(markdown[:excerpt_index], output_format="html5")
+
+        if page.meta.get("rss"):
+            page.meta["rss"]["feed_description"] = excerpt
+        else:
+            page.meta["rss"] = {"feed_description": excerpt}
+
+    return markdown
+
+
+def on_page_content(html, page, config, files):
+    if not include.match(page.url):
+        return html
+    return html
