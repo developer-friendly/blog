bffe4a3d6d7a874905f457484399effa0ff2d29e
Meysam <meysam@developer-friendly.blog>
2025-01-07 11:18:13 +0700

    fix: render captcha explicitly on submit (#68)
    * fix: render captcha explicitly on submit
    
    * chore: reset captcha on xhr failure
    
    * chore: reset before emptying the div
    
    * chore: render and execute separately
    
    * only reset form on success
    
    * chore: render again after reset
    
    * fix: modify broken discord invite link
    
    * chore: render explicitly on form open

--- docs/static/subscribe.js
+++ docs/static/subscribe.js
@@ -3,31 +3,49 @@ document$.subscribe(function prepareSubForm() {
   var subscriptionForm = document.getElementById("subscription-form-b8eb1947");
   var subscribeButton = document.getElementById("subscribe-button-ea4577c9");
   var submitInfo = document.getElementById("subscribe-submit-info-345a25b9");
+  const hcaptchaDivId = "h-captcha-0de6fb2e-eb24-454a-8dfe-4f6c9670ab7e";
+  const hcaptchaSiteKey = "0de6fb2e-eb24-454a-8dfe-4f6c9670ab7e";
+  window.captchaWidget = window.captchaWidget || null;
+
+  function notifyWarning() {
+    submitInfo.classList.add("flash-warning-dc1f9602");
+
+    setTimeout(() => {
+      submitInfo.classList.remove("flash-warning-dc1f9602");
+    }, 50);
+  }
 
   function subscribeButtonClick() {
     isHidden = formParentDiv.classList.contains("hidden");
-    if (!isHidden) {
-      subscriptionForm.reset();
+    if (isHidden) {
       submitInfo.innerHTML = "";
+      if (captchaWidget) {
+        resetCaptcha();
+      }
+      renderCaptcha();
     }
     formParentDiv.classList.toggle("hidden");
+  }
+
+  function resetCaptcha() {
+    hcaptcha.reset(captchaWidget);
+    captchaWidget = null;
+  }
+
+  function renderCaptcha() {
+    document.getElementById(hcaptchaDivId).innerHTML = "";
+    captchaWidget = hcaptcha.render(hcaptchaDivId, {
+      sitekey: hcaptchaSiteKey,
+    });
   }
 
   function subscribeButtonSubmit(event) {
     event.preventDefault();
 
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-
-    console.debug({
-      name,
-      email,
-    });
-
-    if (hcaptcha.getResponse().length == 0) {
-      submitInfo.innerHTML = "Please complete the captcha!";
-      submitInfo.classList.remove("hidden");
+    if (hcaptcha.getResponse(captchaWidget).length == 0) {
+      notifyWarning();
       submitInfo.classList.add("md-banner--warning");
+      submitInfo.innerHTML = "Please complete the captcha!";
       return;
     }
 
@@ -36,19 +54,31 @@ document$.subscribe(function prepareSubForm() {
     var xhr = new XMLHttpRequest();
     xhr.open(subscriptionForm.method, subscriptionForm.action, true);
     xhr.onload = function onloadHandler() {
+      submitInfo.innerHTML = "Submitting...";
       if (xhr.status == 200) {
         submitInfo.classList.remove("md-banner--warning");
         submitInfo.innerHTML = "Subscription successful!";
+        subscriptionForm.reset();
+        var countdown = 3;
+        var countdownInterval = setInterval(function notifySuccess() {
+          submitInfo.innerHTML = "Subscription successful! " + countdown + "s";
+          countdown--;
+          if (countdown < 0) {
+            clearInterval(countdownInterval);
+            formParentDiv.classList.add("hidden");
+          }
+        }, 1000);
       } else {
         submitInfo.classList.add("md-banner--warning");
         submitInfo.innerHTML = "Subscription failed. Please try again.";
+        resetCaptcha();
+        renderCaptcha();
       }
     };
+    xhr.setRequestHeader("accept", "application/json");
     xhr.send(formData);
 
     submitInfo.classList.remove("hidden");
-
-    subscriptionForm.reset();
   }
 
   subscribeButton.addEventListener("click", subscribeButtonClick);
