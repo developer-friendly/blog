b87b2fcab710294e757a9e40c3c847db5ab132a4
Meysam Azad <meysam@developer-friendly.blog>
2025-02-15 11:23:52 +0700

    chore: get rid of google analytics and cookie banner

--- hooks/totalviews.py
+++ hooks/totalviews.py
@@ -5,23 +5,22 @@ import re
 from collections import defaultdict
 from functools import lru_cache
 
-import psycopg2
+import httpx
 
 include = re.compile(r"blog/[1-9].*")
 
-page_view = defaultdict(int)
+
+PLAUSIBLE_BEARER_TOKEN = os.environ["PLAUSIBLE_BEARER_TOKEN"]
 
 logger = logging.getLogger("mkdocs")
 logger.setLevel(logging.INFO)
-
-DSN = os.environ["GA_REPORTS_DSN"]
 
 
 def on_page_markdown(markdown, page, config, files, **kwargs):
     if not include.match(page.url):
         return markdown
 
-    query_page_views()
+    page_view = query_page_views()
 
     # NOTE: to keep backward compatibility, we remove the blog/ prefix
     old_url = page.url.replace("blog/", "")
@@ -36,22 +35,28 @@ def on_page_markdown(markdown, page, config, files, **kwargs):
 
 @lru_cache(maxsize=1)
 def query_page_views():
-    with psycopg2.connect(DSN) as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                SELECT page_path, SUM(views) AS total_views
-                FROM total_views
-                GROUP BY page_path
-                ORDER BY total_views DESC;
-                """
-            )
+    page_view = defaultdict(int)
+    with httpx.Client(
+        base_url="https://analytics.developer-friendly.blog",
+        headers={
+            "authorization": f"Bearer {PLAUSIBLE_BEARER_TOKEN}",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0",
+        },
+    ) as client:
+        response = client.post(
+            "/api/v2/query",
+            json={
+                "site_id": "developer-friendly.blog",
+                "metrics": ["pageviews"],
+                "date_range": "all",
+                "dimensions": ["event:page"],
+                "include": {"imports": True},
+            },
+        )
+        for row in response.json()["results"]:
+            if not (row["metrics"] and row["dimensions"]):
+                logger.warning(f"Invalid row: {row}")
+                continue
+            page_view[row["dimensions"][0]] += row["metrics"][0]
 
-            cols = [desc[0] for desc in cur.description]
-            results = [dict(zip(cols, row)) for row in cur.fetchall()]
-
-            page_view.update(
-                {result["page_path"]: result["total_views"] for result in results}
-            )
-
-            return page_view
+        return page_view
