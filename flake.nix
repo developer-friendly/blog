b70aed90799bb335e89239b5b9c2c4abe19048b1
Meysam Azad <meysam@meysam.io>
2026-02-12 14:21:19 +0700

    chore(dev): add flake config

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.netlify-cli ];
+        };
+      });
+    };
+}
